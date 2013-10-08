90160c1957d6cdedc6687fb0d3bc0f60d68d38df
Jennifer Fong <jen@ednapiranha.com>
2013-10-08 12:14:38 -0700

    fixed google query

--- engines/foursquare.com.js
+++ engines/foursquare.com.js
@@ -17,9 +17,9 @@ module.exports = new (require('../Engine'))({
   "site": "http://www.foursquare.com/",
   "suggestUrl": "http://www.yelp.com/search_suggest/json?prefix={searchTerms}&src=firefox&loc={geo:name}",
   "queryFunc": function (term, location, geolocation) {
-    console.log("FQ QUERY", term, location, geolocation);
+    //console.log("FQ QUERY", term, location, geolocation);
     var d = q.defer();
-    console.log("geolocation", geolocation);
+    //console.log("geolocation", geolocation);
     var lat = geolocation.split(',')[0];
     var lng = geolocation.split(',')[1];
     foursquare.Venues.explore(
@@ -28,7 +28,7 @@ module.exports = new (require('../Engine'))({
       { query : term, limit : 5, near : location, venuePhotos : 1, radius : 4000 },
       null, // accessToken
       function callback (err, results) {
-        console.log("FourSquare", err, results);
+        //console.log("FourSquare", err, results);
         if(err) { d.reject(err); }
         else {
           d.resolve(results);
